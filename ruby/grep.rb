127337fed2cb7423aabf3b304b65f52bc121bc56
NunoAlexandre <nunombalexandre@gmail.com>
2016-05-17 11:31:52 +0100

    ruby version: grep  function

--- ruby/grep.rb
+++ ruby/grep.rb
@@ -1,9 +1,12 @@
+def grep(term, file)
+	File.foreach(file) { |line|
+        puts line if line.include? term
+    }
+end
+
 if ( ARGV.length < 2 )
     puts "usage: grep term file"
 else
-    term = ARGV[0]
-    filepath = ARGV[1]
-    File.foreach(filepath) { |line|
-        puts line if line.contains? term
-    }
+    grep(ARGV[0], ARGV[1])
 end
+
