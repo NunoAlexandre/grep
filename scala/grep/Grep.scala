a6f73e00db2e09a461bfe20d8d509fb75753539b
NunoAlexandre <nunombalexandre@gmail.com>
2016-05-17 11:49:59 +0100

    scala version - naming args values

--- scala/grep/Grep.scala
+++ scala/grep/Grep.scala
@@ -3,11 +3,14 @@ import scala.io.Source
 object Grep {
    def main(args: Array[String]): Unit = {
       if (args.size < 2)
-         println("usage: grep ")
-      else
-         grep(args(0), args(1))
+         println("usage: grep term file")
+      else {
+         val term = args(0)
+         val file = args(1)
+         grep(term,file) 
+      }
    }
-
+   
    def grep(term: String, filepath: String): Unit = {
       Source.fromFile(filepath).getLines().foreach { line =>
          if (line.contains(term))
